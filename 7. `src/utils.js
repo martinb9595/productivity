3ebb1568089d7945c1ead6d33f887ea134a31de9
Martin Boev <83338805+martinb9595@users.noreply.github.com>
2024-10-17 22:44:03 +0300

    dead

--- 7. `src/utils.js
+++
@@ -1,5 +0,0 @@
-export const formatTimeRemaining = (time) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
-};
